780a11fc48f346799b46665ccd4f953d54973fc7
bostond <spammed@gmail.com>
2012-03-06 20:49:28 -0500

    Tighten up object model, requiring parent in contructor. The exception is the MachineRoot class - the root of the Composite's tree.

---
+++ src/main/java/com/oozinoz/machine/MachineRoot.java
@@ -0,0 +1,15 @@
+package com.oozinoz.machine;
+
+/**
+ * 
+ * @author bostond
+ *
+ */
+
+public class MachineRoot extends MachineComposite {
+	
+	public MachineRoot(int id) {
+		super(id, null);
+	}
+
+}
